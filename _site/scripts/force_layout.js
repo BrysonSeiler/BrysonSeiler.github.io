53949880411b5766808c511747f01365e7da9807
Bryson Seiler <Bryson.Seiler@gmail.com>
2018-06-25 16:55:44 -0600

    Changed force_layout.js to allow different node sizing/coloring

--- _site/scripts/force_layout.js
+++ _site/scripts/force_layout.js
@@ -8,11 +8,12 @@
 *    Date: 06/04/2018
 */
 
-
 //Bryson Seiler: Added: function to open json networks:
-function open_file(filename, scale_node, link_strength, body_strength, collide_strength, distance, iterations, show_labels, node_att) {
+function setup(filename, scale_node, link_strength, body_strength, collide_strength, distance, iterations, show_labels, node_att) {
 
 	//Bryson Seiler: Added: Varaibles: filename, scale_node, link_strength, body_strength, collide_strength, distance, iterations
+
+	var option = 0;
 
 	var filename = filename;
 	var scale_node = scale_node;
@@ -28,120 +29,212 @@ function open_file(filename, scale_node, link_strength, body_strength, collide_s
 		width = +svg.attr("width"),
 		height = +svg.attr("height");
 
-	var color = d3.scaleOrdinal().range(["#E63946", "#455561", "#457B9D", "#457B9D", "#1D3557"]);
-
-	//var color = d3.scaleOrdinal().range(["#124e78", "#f0f0c9", "#f2bb05", "#d74e09", "#6e0e0a"]);
+	var color = d3.scaleOrdinal().range(["#457B9D"]);
 
 	var simulation = d3.forceSimulation()
-
 		//Bryson Seiler: Added: 1. Collision 2. 
-
 		.force("link", d3.forceLink().id(function (d) { return d.id; }).strength(link_strength).distance(distance))
 		.force("charge", d3.forceManyBody().strength(body_strength))
 		.force("collision", d3.forceCollide(12).strength(collide_strength).iterations(iterations))
 		.force("center", d3.forceCenter(width / 2, height / 2));
 
-	d3.json(filename, function (error, graph) {
-		if (error) throw error;
+	function draw(filename, scale_node, svg, color, simulation, show_labels, node_att) {
 
-		var link = svg.append("g")
-			.attr("class", "links")
-			.selectAll("line")
-			.data(graph.links)
-			.enter().append("line")
-			.attr("stroke-width", function (d) { return Math.sqrt(d.value); });
+		d3.json(filename, function (error, graph) {
+			if (error) throw error;
 
-		if (node_att) {
+			var link = svg.append("g")
+				.attr("class", "links")
+				.selectAll("line")
+				.data(graph.links)
+				.enter().append("line")
+				.attr("stroke-width", function (d) { return Math.sqrt(d.value); });
 
-			var node = svg.append("g")
-				.attr("class", "nodes")
-				.selectAll("circle")
-				.data(graph.nodes)
-				.enter().append("circle")
-				//Bryson Seiler added: Change color/size based off of degree
-				.attr("fill", function (d) { return color(d.Degree); })
-				.attr("r", function (d) { return d.Degree * scale_node; })
-				.call(d3.drag()
-					.on("start", dragstarted)
-					.on("drag", dragged)
-					.on("end", dragended));
-		}
+			if (node_att) {
 
-		else {
+				if (option == 0) {
 
-			var node = svg.append("g")
-				.attr("class", "nodes")
-				.selectAll("circle")
-				.data(graph.nodes)
-				.enter().append("circle")
-				//Bryson Seiler added: Change color/size based off of degree
-				.attr("fill", "#457B9D")
-				.attr("r", scale_node)
-				.call(d3.drag()
-					.on("start", dragstarted)
-					.on("drag", dragged)
-					.on("end", dragended));
+					var node = svg.append("g")
+						.attr("class", "nodes")
+						.selectAll("circle")
+						.data(graph.nodes)
+						.enter().append("circle")
+						//Bryson Seiler added: Change color/size based off of degree
+						.attr("fill", "#457B9D")
+						.attr("r", 3 * scale_node)
+						.call(d3.drag()
+							.on("start", dragstarted)
+							.on("drag", dragged)
+							.on("end", dragended));
 
-		}
+				}
 
-		if (show_labels == true) {
+				if (option == 1) {
 
-			var lables = svg.append("g")
-				.attr("class", "labels")
-				.selectAll("text")
-				.data(graph.nodes)
-				.enter().append("text")
-				.attr("dx", ".25em")
-				.attr("dy", "-0.5em")
-				.text(function (d) { return d.id; });
-		}
+					var node = svg.append("g")
+						.attr("class", "nodes")
+						.selectAll("circle")
+						.data(graph.nodes)
+						.enter().append("circle")
+						//Bryson Seiler added: Change color/size based off of degree
+						.attr("fill", function (d) { return color(d.Degree); })
+						.attr("r", function (d) { return d.Degree * scale_node; })
+						.call(d3.drag()
+							.on("start", dragstarted)
+							.on("drag", dragged)
+							.on("end", dragended));
+				}
 
-		node.append("title")
-			.text(function (d) { return d.id; });
+				if (option == 2) {
 
-		simulation
-			.nodes(graph.nodes)
-			.on("tick", ticked);
+					var node = svg.append("g")
+						.attr("class", "nodes")
+						.selectAll("circle")
+						.data(graph.nodes)
+						.enter().append("circle")
+						//Bryson Seiler added: Change color/size based off of degree
+						.attr("fill", function (d) { return color(d.BetweennessCentrality); })
+						.attr("r", function (d) { return d.BetweennessCentrality/(2*scale_node); })
+						.call(d3.drag()
+							.on("start", dragstarted)
+							.on("drag", dragged)
+							.on("end", dragended));
+				}
 
-		simulation.force("link")
-			.links(graph.links);
+				if (option == 3) {
 
-		function ticked() {
-			link
-				.attr("x1", function (d) { return d.source.x; })
-				.attr("y1", function (d) { return d.source.y; })
-				.attr("x2", function (d) { return d.target.x; })
-				.attr("y2", function (d) { return d.target.y; });
-
-			node
-				.attr("cx", function (d) { return d.x; })
-				.attr("cy", function (d) { return d.y; });
-
-			if (show_labels == true) {
-				
-				lables
-					.attr("x", function (d) { return d.x; })
-					.attr("y", function (d) { return d.y; });
+					var node = svg.append("g")
+						.attr("class", "nodes")
+						.selectAll("circle")
+						.data(graph.nodes)
+						.enter().append("circle")
+						//Bryson Seiler added: Change color/size based off of degree
+						.attr("fill", function (d) { return color(d.EigenvectorCentrality); })
+						.attr("r", function (d) { return d.EigenvectorCentrality * scale_node * 9; })
+						.call(d3.drag()
+							.on("start", dragstarted)
+							.on("drag", dragged)
+							.on("end", dragended));
+				}
 
 			}
+
+			else {
+
+				var node = svg.append("g")
+					.attr("class", "nodes")
+					.selectAll("circle")
+					.data(graph.nodes)
+					.enter().append("circle")
+					//Bryson Seiler added: Change color/size based off of degree
+					.attr("fill", "#457B9D")
+					.attr("r", scale_node)
+					.call(d3.drag()
+						.on("start", dragstarted)
+						.on("drag", dragged)
+						.on("end", dragended));
+
+			}
+
+			if (show_labels == true) {
+
+				var lables = svg.append("g")
+					.attr("class", "labels")
+					.selectAll("text")
+					.data(graph.nodes)
+					.enter().append("text")
+					.attr("dx", ".25em")
+					.attr("dy", "-0.5em")
+					.text(function (d) { return d.id; });
+			}
+
+			node.append("title")
+				.text(function (d) { return d.id; });
+
+			simulation
+				.nodes(graph.nodes)
+				.on("tick", ticked);
+
+			simulation.force("link")
+				.links(graph.links);
+
+			function ticked() {
+				link
+					.attr("x1", function (d) { return d.source.x; })
+					.attr("y1", function (d) { return d.source.y; })
+					.attr("x2", function (d) { return d.target.x; })
+					.attr("y2", function (d) { return d.target.y; });
+
+				node
+					.attr("cx", function (d) { return d.x; })
+					.attr("cy", function (d) { return d.y; });
+
+				if (show_labels == true) {
+
+					lables
+						.attr("x", function (d) { return d.x; })
+						.attr("y", function (d) { return d.y; });
+
+				}
+			}
+		});
+
+		function dragstarted(d) {
+			if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+			d.fx = d.x;
+			d.fy = d.y;
 		}
-	});
 
-	function dragstarted(d) {
-		if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-		d.fx = d.x;
-		d.fy = d.y;
+		function dragged(d) {
+			d.fx = d3.event.x;
+			d.fy = d3.event.y;
+		}
+
+		function dragended(d) {
+			if (!d3.event.active) simulation.alphaTarget(0);
+			d.fx = null;
+			d.fy = null;
+		}
 	}
 
-	function dragged(d) {
-		d.fx = d3.event.x;
-		d.fy = d3.event.y;
+	window.draw = draw(filename, scale_node, svg, color, simulation, show_labels, node_att);
+
+	function choose(choice) {
+
+		//Reset
+		d3.selectAll("g").remove();
+		
+		simulation = d3.forceSimulation()
+			.force("link", d3.forceLink().id(function (d) { return d.id; }).strength(link_strength).distance(distance))
+			.force("charge", d3.forceManyBody().strength(body_strength))
+			.force("collision", d3.forceCollide(12).strength(collide_strength).iterations(iterations))
+			.force("center", d3.forceCenter(width / 2, height / 2));
+
+		if (choice == 1) {
+			option = 1;
+
+			color = d3.scaleOrdinal().range(["#E63946", "#455561", "#457B9D", "#457B9D", "#1D3557"]);
+			draw(filename, scale_node, svg, color, simulation, show_labels, node_att);
+		}
+
+		if (choice == 2) {
+
+			option = 2;
+
+			color = d3.scaleOrdinal().range(["#132a13", "#31572c", "#4f772d", "#90a955", "#ecf39e"]);
+			draw(filename, scale_node, svg, color, simulation, show_labels, node_att);
+		}
+
+		if (choice == 3) {
+			option = 3;
+			
+			color = d3.scaleOrdinal().range(["#124e78", "#f0f0c9", "#f2bb05", "#d74e09", "#6e0e0a"]);
+			draw(filename, scale_node, svg, color, simulation, show_labels, node_att);
+		}
+
 	}
 
-	function dragended(d) {
-		if (!d3.event.active) simulation.alphaTarget(0);
-		d.fx = null;
-		d.fy = null;
-	}
+	window.choose = choose;
+
 
 }
